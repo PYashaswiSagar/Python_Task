3cafd3b3fa77635040e0429a03095a0021aa4652
PYashaswiSagar <psagar@griddynamics.com>
2025-03-21 17:46:05 +0530

    Update task3.py

--- PYTHON-BASIC/practice/1_python_part_1/task3.py
+++ PYTHON-BASIC/practice/1_python_part_1/task3.py
@@ -12,8 +12,22 @@ Examples:
     >>> build_from_unique_words(word_number=10)
     ''
 """
-from typing import Iterable
+# from typing import Iterable
 
 
-def build_from_unique_words(*lines: Iterable[str], word_number: int) -> str:
-    ...
+# def build_from_unique_words(*lines: Iterable[str], word_number: int) -> str:
+#     ...
+
+def build_from_unique_words(*lines, word_number):
+    result_words = []
+    for i in lines:
+        unique_words = list(dict.fromkeys(i.split())) 
+        if word_number < len(unique_words):  
+            result_words.append(unique_words[word_number])  
+    return " ".join(result_words)  
+    
+print(build_from_unique_words('a b c', '1 1 1 2 3', 'cat dog milk', word_number=1)) 
+print(build_from_unique_words('a b c', '', 'cat dog milk', word_number=0))  
+print(build_from_unique_words('1 2', '1 2 3', word_number=10)) 
+print(build_from_unique_words(word_number=10)) 
+
