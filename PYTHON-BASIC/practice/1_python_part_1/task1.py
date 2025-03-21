30d575ab000e4b0d0112f145cb6202f14aae29e1
PYashaswiSagar <psagar@griddynamics.com>
2025-03-21 11:35:27 +0530

    Update task1.py

--- PYTHON-BASIC/practice/1_python_part_1/task1.py
+++ PYTHON-BASIC/practice/1_python_part_1/task1.py
@@ -11,8 +11,16 @@ Examples:
     >>> delete_from_list([], 'b')
     []
 """
-from typing import List, Any
+# from typing import List, Any
 
 
-def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
+# def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
     ...
+
+def delete_from_list(lst, element):
+  return [item for item in lst if item != element]
+    
+print(delete_from_list([1, 2, 3, 4, 3], 3)) 
+print(delete_from_list(['a', 'b', 'c', 'b', 'd'], 'b')) 
+print(delete_from_list([1, 2, 3], 'b'))  
+print(delete_from_list([], 'b'))
